b2e10c4259c36315eb2c2dab16f8e1e62d0a099c
M-Hasbi <kaynakhasbi1@gmail.com>
2022-01-28 14:16:12 +0100

    create : Service file and IService.cs

---
+++ NLayer.Core/Service/IService.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Service
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> Any(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task RemoveRange(IEnumerable<T> entities);
+    }
+}
