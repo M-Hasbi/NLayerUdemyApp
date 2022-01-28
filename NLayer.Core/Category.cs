7e6b7823462769da879a7781945ccefd0f285eb9
M-Hasbi <kaynakhasbi1@gmail.com>
2022-01-28 13:33:18 +0100

    add : nullable fields into the props

--- NLayer.Core/Category.cs
+++ NLayer.Core/Category.cs
@@ -2,8 +2,8 @@
 {
     public class Category:BaseEntity
     {
-        public string Name { get; set; }
-        public ICollection<Product> Products { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Product>? Products { get; set; }
 
     }
 }
