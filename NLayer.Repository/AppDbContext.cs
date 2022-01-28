151806711735acbab5d5fd65926c9bc245ef29cf
M-Hasbi <kaynakhasbi1@gmail.com>
2022-01-28 17:36:30 +0100

    create : Configuration file and CategoryConfiguration.cs

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace NLayer.Repository
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
