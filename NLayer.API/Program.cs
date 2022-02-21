b3147cba50bd9afbc1e295a6db5a56cf25bae2ba
M-Hasbi <kaynakhasbi1@gmail.com>
2022-02-21 14:19:11 +0100

    implement CachingService

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
 builder.Services.AddAutoMapper(typeof(MapProfile));
