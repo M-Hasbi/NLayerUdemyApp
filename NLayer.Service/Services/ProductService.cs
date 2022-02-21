4d15f27debe7529e4e0dd7aa553ab3b32299d6fc
M-Hasbi <kaynakhasbi1@gmail.com>
2022-02-21 17:23:18 +0100

    create productcontroller unload API project fix bugs

--- NLayer.Service/Services/ProductService.cs
+++ NLayer.Service/Services/ProductService.cs
@@ -7,22 +7,22 @@ using NLayer.Core.UnitOfWorks;
 
 namespace NLayer.Service.Services
 {
-    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    public class ProductService : Service<Product>, IProductService
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
-        public ProductServiceWithNoCaching(IGenericRepository<Product> repo, IUnitOfWork unitOfWork, IProductRepository productRepository, IMapper mapper) : base(repo, unitOfWork)
+        public ProductService(IGenericRepository<Product> repo, IUnitOfWork unitOfWork, IProductRepository productRepository, IMapper mapper) : base(repo, unitOfWork)
         {
             _productRepository = productRepository;
             _mapper = mapper;
         }
 
-        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        public async Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
         {
             List<Product>? products = await _productRepository.GetProductsWithCategory();
             List<ProductWithCategoryDto>? productsDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
 
-            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
+            return productsDto;
         }
     }
 }
